c0d60558ad9856f95626e047cf4e19b1b25e1a6b
Mongolmaa Gurbazar <mongolmaa555@gmail.com>
2020-11-06 18:23:13 -0500

    imported express, burger & created router and exported it

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,26 +1,46 @@
 //Dependencies
 var express = require("express");
-var exphbs = require("express-handlebars");
+
 var burger = require("../models/burger.js");
 
-// Create an instance of our the express app
-var app = express();
+// Create an instance of for the express.Router app
+var router = express.Router();
 
-// Set the port of my application
-// process.env.PORT lets the port to be set by Heroku
-var PORT = process.env.PORT || 8080;
-
-// Set handlebars as the default templating engine
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-//routes
-app.get("/", function (req, res) {
-  res.render("burgers", { burgers: burger });
+// Create all the routes and set up logic within those routes where required
+router.get("/", function (req, res) {
+  burger.all("/", function (data) {
+    var hbsObject = { burgers: data };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
 });
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function () {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+router.post("/api/burgers", function (req, res) {
+  burger.create(["burger"], [req.body.burger], function (result) {
+    //Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
 });
+
+// router.put("/api/burgers/:id", function (req, res) {
+//   var condition = "id = " + req.params.id;
+
+//   console.log("condition", condition);
+
+//   burger.update(
+//     {
+//       devour: req.body.devour,
+//     },
+//     condition,
+//     function (result) {
+//       if (result.changedRows === 0) {
+//         //If no rows were changed, then the ID must not exist, so 404
+//         return res.status(404).end();
+//       }
+//       res.status(200).end();
+//     }
+//   );
+// });
+
+//Export routes for burger.js to use
+module.exports = router;
