281d095e3e9454c1c1dc457e6d3f50840fe24e01
Mongolmaa Gurbazar <mongolmaa555@gmail.com>
2020-11-08 13:08:22 -0500

    it creates new order :)

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,26 +1,7 @@
 $(function () {
-  $(".create-form").on("click", function (event) {
+  $("#submit").on("click", function (event) {
     // Make sure to preventDefault on a submit event
     event.stopPropagation();
-
-    var newBurger = {
-      burger: $("#brgr").val().trim(),
-    };
-
-    //Send the POST request
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newBurger,
-    }).then(function () {
-      console.log("created new burger");
-      //Reload the page tp get the updated list
-      location.reload();
-    });
-  });
-  $("#brgr").on("click", function (event) {
-    // Make sure to preventDefault on a submit event
-    event.stopPropagation();
-    alert("clicked");
 
     var newBurger = {
       burger: $("#brgr").val().trim(),
